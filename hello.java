5bca9919f00146eecdb9d576e81e2b77a831f0f7
samikotwal <samikotwal15@gmail.com>
2024-05-15 18:33:46 +0530

    sgg

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class hello{
     public static void main(String [] args){
-        system.out.println("Hello");
-        system.out.println("hi");
+        System.out.println("Hello");
+        System.out.println("hi");
     }
 }
